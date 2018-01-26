3c7974f7ffaa1fd7699fce598d5193ec761e0a35
Jambrose777 <Jaa74458@uga.edu>
2018-01-26 11:28:53 -0500

    fixed results format

--- ClockLuck/src/ClockLuck.cpp
+++ ClockLuck/src/ClockLuck.cpp
@@ -171,7 +171,7 @@ int main(int argc, char * argv[]){
 	save.open("results/Results.md");
 	int avg = 0, count = ScrambleList.size();
 	ScrambleList.sort(comp);
-	save << "#**Clock Luck in a Scramble Set**\n\n|Competition|Round|Group|1|Luck|\n|--------|--------|--------|--------|--------|" << endl;
+	save << "# **Clock Luck in a Scramble Set**\n\n|Competition|Round|Group|1|Luck|\n|--------|--------|--------|--------|--------|" << endl;
 	for(Scramble * s : ScrambleList){
 		avg+=s->all;
 		save << s->comp << s->all << "|" << endl;
