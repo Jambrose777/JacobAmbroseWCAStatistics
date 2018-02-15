62d747c1bdd31086642c726d43e658e09248f307
Jambrose777 <Jaa74458@uga.edu>
2018-02-15 15:55:30 -0500

    fixed grouping by person's year to competition's year

--- MostCompsInYear/query.sql
+++ MostCompsInYear/query.sql
@@ -1,7 +1,7 @@
 SELECT CONCAT('[', p.name, '](https://www.worldcubeassociation.org/persons/', r.personId, ')') person, 
-     RIGHT(competitionId, 4) as year, 
-     COUNT(DISTINCT competitionId) AS comps 
+     RIGHT(r.competitionId, 4) as year, 
+     COUNT(DISTINCT r.competitionId) AS comps 
 FROM Results r
 INNER JOIN Persons p ON r.personId = p.id
-GROUP BY r.personId, year
+GROUP BY r.personId, RIGHT(r.competitionId, 4)
 ORDER BY comps DESC
