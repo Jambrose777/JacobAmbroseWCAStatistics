aeb954b48abd45fffe367084e7461ea8836b9155
Jambrose777 <Jaa74458@uga.edu>
2018-02-15 14:01:00 -0500

    Update and rename query.md to query.sql

--- PotentialFMCAvg/query.sql
+++ PotentialFMCAvg/query.sql
@@ -1,4 +1,4 @@
-~~~sql
+
 SELECT CONCAT('[', personName, '](https://www.worldcubeassociation.org/persons/', personId, ')') person, CONCAT('[', competitionId, '](https://www.worldcubeassociation.org/competitions/', competitionId, ')'), value1, value2, value3, IF(average < 8100, average, IF(average = 8100, 'N/A', 'DNF')), (value1+value2)/2 AS potential
 FROM 
   (SELECT personName, personId, competitionId, value1, value2, value3, IF(average > 0, average, IF(average = 0, 8100, 8200)) as average
@@ -6,4 +6,3 @@ FROM
   WHERE eventId = '333fm' AND value1 > 0 AND value2 > 0
   ) temp
 ORDER BY potential, average
-~~~
