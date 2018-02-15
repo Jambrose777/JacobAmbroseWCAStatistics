a7fa175531ba0a325ef6df0482303dd25930577d
Jambrose777 <Jaa74458@uga.edu>
2018-02-15 14:02:21 -0500

    changed file type

--- PotentialFMCAvg/query.sql
+++ PotentialFMCAvg/query.sql
@@ -1,4 +1,3 @@
-
 SELECT CONCAT('[', personName, '](https://www.worldcubeassociation.org/persons/', personId, ')') person, CONCAT('[', competitionId, '](https://www.worldcubeassociation.org/competitions/', competitionId, ')'), value1, value2, value3, IF(average < 8100, average, IF(average = 8100, 'N/A', 'DNF')), (value1+value2)/2 AS potential
 FROM 
   (SELECT personName, personId, competitionId, value1, value2, value3, IF(average > 0, average, IF(average = 0, 8100, 8200)) as average
