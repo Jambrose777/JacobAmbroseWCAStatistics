6254897cf409c480ef693e534be7d8ec8b341847
Jambrose777 <Jaa74458@uga.edu>
2018-02-15 14:49:48 -0500

    fixed spacing

--- MostStates/query.sql
+++ MostStates/query.sql
@@ -5,14 +5,14 @@ SELECT
      GROUP_CONCAT(state) AS listStates
 FROM 
     (SELECT CONCAT(SUBSTR(c.cityName, LOCATE(',', c.cityName)+1), ' (', COUNT(*), ')') AS state, r.personId, p.name
- 	   FROM 
+ 	FROM 
          (SELECT personId, competitionId
           FROM Results 
           GROUP BY personId, competitionId
          )r
      INNER JOIN Competitions c ON r.competitionId = c.id
      INNER JOIN Persons p ON r.personId = p.id
- 	   WHERE c.countryId = 'USA' AND SUBSTR(c.cityName, LOCATE(',', c.cityName)+1) != 'Multiple Cities'
+     WHERE c.countryId = 'USA' AND SUBSTR(c.cityName, LOCATE(',', c.cityName)+1) != 'Multiple Cities'
     	GROUP BY SUBSTR(c.cityName, LOCATE(',', c.cityName)+1), r.personId
     )s
 INNER JOIN 
