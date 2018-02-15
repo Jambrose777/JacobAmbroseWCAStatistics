3f4c338edd7510820eab5149a4434839ae25788c
Jambrose777 <Jaa74458@uga.edu>
2018-02-15 15:44:58 -0500

    fixed name with link

--- MostCompsInYear/query.sql
+++ MostCompsInYear/query.sql
@@ -1,4 +1,7 @@
-SELECT personName, RIGHT(competitionId, 4) as year, COUNT(DISTINCT competitionId) AS comps 
-FROM Results 
-GROUP BY personName, year
-ORDER BY comps DESC Limit 100
+SELECT CONCAT('[', p.personName, '](https://www.worldcubeassociation.org/persons/', r.personId, ')') person, 
+     RIGHT(competitionId, 4) as year, 
+     COUNT(DISTINCT competitionId) AS comps 
+FROM Results r
+INNER JOIN Persons p ON r.personId = p.id
+GROUP BY r.personId, year
+ORDER BY comps DESC
