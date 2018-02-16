6e503ef6c6ef739f145e879038cf8a0a8220a794
Jambrose777 <Jaa74458@uga.edu>
2018-02-16 12:26:26 -0500

    fixed persons issue

--- MostCompsInYear/query.sql
+++ MostCompsInYear/query.sql
@@ -2,6 +2,10 @@ SELECT CONCAT('[', p.name, '](https://www.worldcubeassociation.org/persons/', r.
      RIGHT(r.competitionId, 4) as InYear, 
      COUNT(DISTINCT r.competitionId) AS comps 
 FROM Results r
-INNER JOIN Persons p ON r.personId = p.id
+INNER JOIN 
+    (SELECT id, name
+     FROM Persons
+     WHERE subId = 1
+    )p ON r.personId = p.id
 GROUP BY r.personId, InYear
 ORDER BY comps DESC, InYear
