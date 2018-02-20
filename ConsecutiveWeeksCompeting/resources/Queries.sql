44c4222837298306daed978179a93693e9e86696
Jambrose777 <Jaa74458@uga.edu>
2018-02-20 11:50:06 -0500

    added competition name

--- ConsecutiveWeeksCompeting/resources/Queries.sql
+++ ConsecutiveWeeksCompeting/resources/Queries.sql
@@ -1,4 +1,4 @@
-SELECT r.personId, r.personName, r.competitionId, c.start_date, c.end_date
+SELECT r.personId, r.personName, r.competitionId, c.name, c.start_date, c.end_date
 FROM Results r
 INNER JOIN Competitions c ON r.competitionId = c.id
 GROUP BY r.personId, r.competitionId
