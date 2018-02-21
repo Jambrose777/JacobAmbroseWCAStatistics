4f7181b4968c80e841807d8d0d0764b898819d6d
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-20 21:13:08 -0500

    added appropriate spacing

--- PotentialFMCAvg/query.sql
+++ PotentialFMCAvg/query.sql
@@ -1,7 +1,23 @@
-SELECT CONCAT('[', personName, '](https://www.worldcubeassociation.org/persons/', personId, ')') person, CONCAT('[', competitionId, '](https://www.worldcubeassociation.org/competitions/', competitionId, ')'), value1, value2, value3, IF(average < 8100, average, IF(average = 8100, 'N/A', 'DNF')), (value1+value2)/2 AS potential
+SELECT 
+     CONCAT('[', personName, '](https://www.worldcubeassociation.org/persons/', personId, ')') person, 
+     CONCAT('[', competitionId, '](https://www.worldcubeassociation.org/competitions/', competitionId, ')') competition, 
+     value1, 
+     value2, 
+     value3, 
+     IF(average < 8100, average, IF(average = 8100, 'N/A', 'DNF')) mean, 
+     (value1+value2)/2 AS potential
 FROM 
-  (SELECT personName, personId, competitionId, value1, value2, value3, IF(average > 0, average, IF(average = 0, 8100, 8200)) as average
-  FROM Results
-  WHERE eventId = '333fm' AND value1 > 0 AND value2 > 0
-  ) temp
+    (SELECT 
+          personName, 
+          personId, 
+          competitionId, 
+          value1, 
+          value2,
+          value3, 
+          IF(average > 0, average, IF(average = 0, 8100, 8200)) as average
+     FROM Results
+     WHERE eventId = '333fm' 
+          AND value1 > 0 
+          AND value2 > 0
+    )temp
 ORDER BY potential, average
