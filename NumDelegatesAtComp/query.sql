5000dc515b23a14561d083ec9cf6789526fd4cc2
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-20 21:30:24 -0500

    Update query.sql

--- NumDelegatesAtComp/query.sql
+++ NumDelegatesAtComp/query.sql
@@ -1,6 +1,10 @@
 SELECT competitionId, COUNT(DISTINCT personId) as delegates
 FROM Results
-WHERE personId IN (SELECT wca_id FROM `users` WHERE delegate_status IS NOT NULL)
+WHERE personId IN 
+    (SELECT wca_id 
+     FROM `users` 
+     WHERE delegate_status IS NOT NULL
+    )
 GROUP BY CompetitionId
 ORDER BY delegates DESC
 LIMIT 100
