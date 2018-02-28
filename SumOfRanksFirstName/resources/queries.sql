c0e1ad6aebb1dad009b16bbff7eb5b03ae40831e
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-28 17:18:07 -0500

    added average queries

--- SumOfRanksFirstName/resources/queries.sql
+++ SumOfRanksFirstName/resources/queries.sql
@@ -1,4 +1,4 @@
-#All ranks for a first name
+#All Single ranks for a first name
 SELECT p.name, p.id, RanksSingle.eventId, RanksSingle.worldRank
 FROM RanksSingle
 INNER JOIN Persons p ON RanksSingle.personId = p.id
@@ -10,7 +10,24 @@ WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)),
     )
 ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId;
 
-#worst rankings
+#worst Single rankings
 SELECT r.eventId, Max(r.worldRank) + 1
 FROM RanksSingle r
+GROUP BY r.eventId;
+
+#All Average ranks for a first name
+SELECT p.name, p.id, RanksAverage.eventId, RanksAverage.worldRank
+FROM RanksAveraeg
+INNER JOIN Persons p ON RanksAverage.personId = p.id
+WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId, RanksSingle.worldRank) IN (
+	SELECT IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, MIN(r.worldRank) as rank
+	FROM RanksSingle r
+	INNER JOIN Persons p ON r.personId = p.id
+	GROUP BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
+    )
+ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksAverage.eventId;
+
+#worst Average rankings
+SELECT r.eventId, Max(r.worldRank) + 1
+FROM RanksAverage r
 GROUP BY r.eventId;
