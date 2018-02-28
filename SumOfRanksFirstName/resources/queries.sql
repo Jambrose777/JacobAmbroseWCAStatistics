d7ead0afb6eea2f1fd957e0968d2d0bf0cf9e78f
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-28 12:43:43 -0500

    subbed in r with RanksSingle

--- SumOfRanksFirstName/resources/queries.sql
+++ SumOfRanksFirstName/resources/queries.sql
@@ -1,14 +1,14 @@
 #All ranks for a first name
-SELECT p.name, p.id, r.eventId, r.worldRank
-FROM RanksSingle r
-INNER JOIN Persons p ON r.personId = p.id
-WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, r.worldRank) IN (
+SELECT p.name, p.id, RanksSingle.eventId, RanksSingle.worldRank
+FROM RanksSingle
+INNER JOIN Persons p ON RanksSingle.personId = p.id
+WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId, RanksSingle.worldRank) IN (
 	SELECT IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, MIN(r.worldRank) as rank
 	FROM RanksSingle r
 	INNER JOIN Persons p ON r.personId = p.id
 	GROUP BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
     )
-ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId;
+ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId;
 
 #worst rankings
 SELECT r.eventId, Max(r.worldRank) + 1
