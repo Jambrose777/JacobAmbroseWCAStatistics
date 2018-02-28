bfad66cdd327cce8ec70b038bc04e122de3ec1ad
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-28 12:26:25 -0500

    removed Daniel Check

--- SumOfRanksFirstName/resources/queries.sql
+++ SumOfRanksFirstName/resources/queries.sql
@@ -6,10 +6,9 @@ WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)),
 	SELECT IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, MIN(r.worldRank) as rank
 	FROM RanksSingle r
 	INNER JOIN Persons p ON r.personId = p.id
-WHERE IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)) = 'Daniel'
 	GROUP BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
     )
-ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
+ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId;
 
 #worst rankings
 SELECT r.eventId, Max(r.worldRank) + 1
