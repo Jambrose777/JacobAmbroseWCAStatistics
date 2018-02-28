3c0c43121c8eb0509b4a6583cc1f1f880408bacb
Jacob Ambrose <Jaa74458@uga.edu>
2018-02-28 12:00:53 -0500

    created query 1

--- SumOfRanksFirstName/resources/queries.sql
+++ SumOfRanksFirstName/resources/queries.sql
@@ -1,2 +1,5 @@
-.
-
+SELECT p.name, p.id, r.eventId, MIN(r.worldRank)
+FROM RanksSingle r
+INNER JOIN Persons p ON r.personId = p.id
+GROUP BY r.eventId, LEFT(p.name,LOCATE(' ',p.name) - 1)
+ORDER BY LEFT(p.name,LOCATE(' ',p.name) - 1), r.eventId
