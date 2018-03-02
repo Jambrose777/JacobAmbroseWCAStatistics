05f0df8160b295f2a17b34c7e5abf934d80d0283
Jacob Ambrose <Jaa74458@uga.edu>
2018-03-01 19:39:35 -0500

    modified queries for letters

--- SumOfRanksFirstLetter/resources/queries.sql
+++ SumOfRanksFirstLetter/resources/queries.sql
@@ -1,31 +1,31 @@
-#All Single ranks for a first name
+#All Single ranks for a first letter
 SELECT p.name, p.id, RanksSingle.eventId, RanksSingle.worldRank
 FROM RanksSingle
 INNER JOIN Persons p ON RanksSingle.personId = p.id
-WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId, RanksSingle.worldRank) IN (
-	SELECT IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, MIN(r.worldRank) as rank
+WHERE LEFT(p.name, 1), RanksSingle.eventId, RanksSingle.worldRank) IN (
+	SELECT LEFT(p.name, 1), r.eventId, MIN(r.worldRank) as rank
 	FROM RanksSingle r
 	INNER JOIN Persons p ON r.personId = p.id
-	GROUP BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
+	GROUP BY LEFT(p.name, 1), r.eventId
     )
-ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksSingle.eventId;
+ORDER BY LEFT(p.name, 1), RanksSingle.eventId;
 
 #worst Single rankings
 SELECT r.eventId, Max(r.worldRank) + 1
 FROM RanksSingle r
 GROUP BY r.eventId;
 
-#All Average ranks for a first name
+#All Average ranks for a first letter
 SELECT p.name, p.id, RanksAverage.eventId, RanksAverage.worldRank
 FROM RanksAverage
 INNER JOIN Persons p ON RanksAverage.personId = p.id
-WHERE (IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksAverage.eventId, RanksAverage.worldRank) IN (
-	SELECT IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId, MIN(r.worldRank) as rank
+WHERE LEFT(p.name, 1), RanksAverage.eventId, RanksAverage.worldRank) IN (
+	SELECT LEFT(p.name, 1), r.eventId, MIN(r.worldRank) as rank
 	FROM RanksAverage r
 	INNER JOIN Persons p ON r.personId = p.id
-	GROUP BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), r.eventId
+	GROUP BY LEFT(p.name, 1), r.eventId
     )
-ORDER BY IF(LOCATE(' ',p.name) = 0, p.name, LEFT(p.name, LOCATE(' ',p.name) - 1)), RanksAverage.eventId;
+ORDER BY LEFT(p.name, 1), RanksAverage.eventId;
 
 #worst Average rankings
 SELECT r.eventId, Max(r.worldRank) + 1
