d92a26e9d78cbc4061647329fe68b503de0309d1
Jacob Ambrose <Jaa74458@uga.edu>
2018-03-23 10:50:40 -0400

    Started Query

--- QualifyUSNats2018/query.sql
+++ QualifyUSNats2018/query.sql
@@ -2,16 +2,16 @@ SELECT competitionId, COUNT(*) AS numQualify
 FROM 
  (SELECT competitionId, personId, eventId, countryId
   FROM
-   (SELECT competitionId, personId, eventId, Competitions.countryId
+   (SELECT competitionId, personId, eventId, Competitions.countryId, Competitions.start_date
     FROM Results
     INNER JOIN Competitions ON Results.competitionId = Competitions.id
     WHERE eventId = '333' AND average <= 3000 AND average > 0
     UNION
-    SELECT competitionId, personId, eventId, Competitions.countryId
+    SELECT competitionId, personId, eventId, Competitions.countryId, Competitions.start_date
     FROM Results
     INNER JOIN Competitions ON Results.competitionId = Competitions.id
     WHERE eventId = '222' AND average <= 1200 AND average > 0
-    ORDER BY Competitions.start_date
+    ORDER BY start_date
     ) b
   GROUP BY personId, eventId
   ) a
