46357daef879ec8dca609add6e2e400e0fc7f7ff
Jacob Ambrose <Jaa74458@uga.edu>
2018-03-23 10:48:30 -0400

    Update query.sql

--- QualifyUSNats2018/query.sql
+++ QualifyUSNats2018/query.sql
@@ -1,1 +1,19 @@
-q
+SELECT competitionId, COUNT(*) AS numQualify
+FROM 
+ (SELECT competitionId, personId, eventId, countryId
+  FROM
+   (SELECT competitionId, personId, eventId, Competitions.countryId
+    FROM Results
+    INNER JOIN Competitions ON Results.competitionId = Competitions.id
+    WHERE eventId = '333' AND average <= 3000 AND average > 0
+    UNION
+    SELECT competitionId, personId, eventId, Competitions.countryId
+    FROM Results
+    INNER JOIN Competitions ON Results.competitionId = Competitions.id
+    WHERE eventId = '222' AND average <= 1200 AND average > 0
+    ORDER BY Competitions.start_date
+    ) b
+  GROUP BY personId, eventId
+  ) a
+WHERE competitionId LIKE '%2018' AND countryId = 'USA'
+GROUP BY competitionId;
