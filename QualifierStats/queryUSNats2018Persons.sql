36df77b11947ee1790d482b0357dc905e5c40a1a
Jacob Ambrose <Jaa74458@uga.edu>
2018-03-23 12:22:26 -0400

    fixed for persons

--- QualifierStats/queryUSNats2018Persons.sql
+++ QualifierStats/queryUSNats2018Persons.sql
@@ -1,96 +1,94 @@
 
-SELECT CONCAT("[", name, "](https://www.worldcubeassociation.org/competitions/", id, ")"), MAX(numQualify) AS numQualifys
-FROM
- (SELECT name, competitionId AS id, COUNT(*) AS numQualify
+SELECT CONCAT("[", personName, "](https://www.worldcubeassociation.org/competitions/", personid, ")"), COUNT(*) AS numQualify
   FROM 
-   (SELECT competitionId, personId, eventId, countryId, name, start_date
+   (SELECT competitionId, personId, eventId, countryId, personName, start_date
     FROM
-     (SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+     (SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333' AND r.average <= 3000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '222' AND r.average <= 1200 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '444' AND r.average <= 70000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '555' AND r.average <= 11000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '666' AND r.average <= 21000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '777' AND r.average <= 28500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333bf' AND r.best <= 24000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333fm' AND r.average <= 4500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333oh' AND r.average <= 3500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333ft' AND r.average <= 7500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = 'clock' AND r.average <= 1500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = 'minx' AND r.average <= 10500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = 'pyram' AND r.average <= 1200 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = 'skewb' AND r.average <= 1200 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = 'sq1' AND r.average <= 3500 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '444bf' AND r.best <= 60000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '555bf' AND r.best <= 120000 AND r.average > 0
       UNION
-      SELECT r.competitionId, r.personId, r.eventId, c.countryId, c.start_date, c.name
+      SELECT r.competitionId, r.personId, r.eventId, r.countryId, c.start_date, r.personName
       FROM Results r
       INNER JOIN Competitions c ON r.competitionId = c.id
       WHERE r.eventId = '333mbf' AND r.best <= '939999999' AND r.average > 0
@@ -99,11 +97,5 @@ FROM
     GROUP BY personId, eventId
     ) a
   WHERE countryId = 'USA' AND start_date > '2017-12-22' AND start_date < '2018-06-18'
-  GROUP BY competitionId
-  UNION 
-  SELECT name, id, 0 AS numQualify
-  FROM Competitions
-  WHERE countryId = 'USA' AND start_date > '2017-12-22' AND start_date < '2018-06-18'
- ) c
-GROUP BY id
-ORDER BY MAX(numQualify) DESC, id;
+  GROUP BY personId
+  ORDER BY numQualify
