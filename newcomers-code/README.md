838b6f46b3fec1f8057ffb9baaad198d82b00293
Jacob Ambrose <jambrose@worldcubeassociation.org>
2022-06-07 13:41:26 -0400

    added fastest newcomer script

---
+++ newcomers-code/README.md
@@ -0,0 +1,5 @@
+Very Rudamentary code to handle this
+
+Install Node JS
+
+run `node app.js`
---
+++ newcomers-code/app.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const competitionId = 'WyomingSummer2022';
+const Authorization = '';
+
+const getLiveResults = async () => {
+  try {
+    const config = {
+      method: 'get',
+      url: 'https://www.worldcubeassociation.org/api/v0/competitions/'+competitionId+'/wcif',
+      headers: { 'Authorization': Authorization }
+    }
+    return await axios(config);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const convertTime = (milliseconds) => {
+  if (milliseconds === -1) return "DNF";                  
+  var seconds = Math.floor((milliseconds) / 100);
+  var minutes = Math.floor((seconds) / 60);
+  var milliseconds = milliseconds % 100;                                       
+  return (!!minutes ? `${minutes}:` : "") + `${seconds}.${milliseconds.toString().padStart(2, '0')}`                                        
+}      
+
+const calculateFastestNewcomers = async () => {
+  const response = [];
+  const liveResults = await getLiveResults();
+
+  if (liveResults.data) {
+    let persons = liveResults.data.persons;
+    results3x3 = liveResults.data.events.find(event => event.id === '333');
+    if (results3x3) {
+      results3x3.rounds.slice().reverse().forEach(r => {
+        r.results.forEach(result => {
+          let person = persons.find(p => p.registrantId === result.personId);
+          if(person.wcaId === null && !response.find(rs => rs.name === person.name)) {
+            response.push({name: person.name, result: convertTime(result.average), round: r.id})
+          }
+        })
+      })
+    }
+  }
+
+  fs.writeFile('./export.json', JSON.stringify(response), err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+calculateFastestNewcomers();
